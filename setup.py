208f29c13b43e1474bef2cac66404b154d1159ee
Martin Melin <martin@tictail.com>
2013-07-09 16:59:13 +0200

    Put requirements.txt in setup.py install_requires, too

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     packages=['githubmirror', ],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.md').read(),
+    install_requires=open('requirements.txt').read(),
     entry_points={
         'console_scripts': ['github-mirror = githubmirror:cmd'],
     }
