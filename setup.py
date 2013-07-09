4f2414bb88789b838eea897bba97f1eb41193bc4
Martin Melin <martin@tictail.com>
2013-07-09 18:10:59 +0200

    Update setup.py and manifest to make pip happy

--- setup.py
+++ setup.py
@@ -5,8 +5,12 @@ setup(
     version='0.1',
     packages=['githubmirror', ],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    description='A small script to keep a copy of every repo in a Github org.',
     long_description=open('README').read(),
     install_requires=open('requirements.txt').read(),
+    author='Martin Melin',
+    author_email='martin@tictail.com',
+    url='http://github.com/martinmelin/githubmirror',
     entry_points={
         'console_scripts': ['github-mirror = githubmirror:cmd'],
     }
